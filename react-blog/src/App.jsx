4dda0977acc81cca7e92d905d545acbb0c2edb45
hariotiwari003 <hariompstiwari@gmail.com>
2026-03-28 12:58:48 +0530

    Controlled Component

--- react-blog/src/App.jsx
+++ react-blog/src/App.jsx
@@ -106,39 +106,85 @@
 
 
 
-import Wrapper from "./wrapper";
+// import Wrapper from "./wrapper";
 
-function App() {
-  return (
-    <div>
-      <h1>Props in React</h1>
-      <Wrapper >
-        <h1>hello everyone</h1>
-        </Wrapper>
+// function App() {
+//   return (
+//     <div>
+//       <h1>Props in React</h1>
+//       <Wrapper >
+//         <h1>hello everyone</h1>
+//         </Wrapper>
         
-         <Wrapper >
-        <h1>hello CODERS!</h1>
-        </Wrapper>
+//          <Wrapper >
+//         <h1>hello CODERS!</h1>
+//         </Wrapper>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+// -------------------Day 3 ------------------------
+
+
+
+
+// import { useState } from "react";
+
+// function App(){
+//   const[val,setval] = useState("");
+
+//   return(
+//     <div>
+//       <h1>get the value</h1>
+//       <input type="text" value={val} onChange={(event)=>setval(event.target.value)} placeholder="enter the text"/>
+//       <h1>{val}</h1>
+//       <button onClick={() =>setval("")}>Clear Value</button>
+//     </div>
+//   )
+// }
+// export default App;
+
+
+import { useState } from "react";
+
+function App(){
+ 
+const [name, setName] = useState('');
+const [password, setPassword] = useState('');
+const [email, setEmail] = useState('');
+  return(
+    <div>
+      <h1>Controller Component</h1>
+      <form action="" method="get">
+      <input type="text"  value={name} onChange={(event)=>setName(event.target.value)} placeholder="Enter the text" />
+         <br />
+        <br />
+        <input type="text" value={password}onChange={(event)=>setPassword(event.target.value)} placeholder="Enter password" />
+         <br />
+        <br />
+
+        <input type="text" value={email} onChange={(event)=>setEmail(event.target.value)} placeholder="first email"  />
+        <br />
+        <br />
+        <button>Submit</button>
+
+        <button onClick={()=>{setEmail('');setPassword('') ; setEmail('')}}>Clear</button>
+        <h3>{name}</h3>
+        <h3>{password}</h3>
+        <h3>{email}</h3>
+      </form>
+
     </div>
-  );
+  )
 }
-
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
